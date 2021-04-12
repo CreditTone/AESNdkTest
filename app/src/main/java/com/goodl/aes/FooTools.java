c010a28863e7a219c599e42c79eddad244face00
郭钟 <guozhong@quicklyun.com>
2021-04-12 14:29:51 +0800

    fix

--- app/src/main/java/com/goodl/aes/FooTools.java
+++ app/src/main/java/com/goodl/aes/FooTools.java
@@ -1,5 +1,13 @@
 package com.goodl.aes;
-
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.HostNameResolver;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 
 public class FooTools {
 
@@ -7,6 +15,8 @@ public class FooTools {
     static {
         System.loadLibrary("fooLib");
     }
+
+    private final String finalText = "123";
 
     /**
      * AES加密, CBC, PKCS5Padding
--- app/src/main/java/guozhong/ndk/test/MainActivity.java
+++ app/src/main/java/guozhong/ndk/test/MainActivity.java
@@ -10,35 +10,84 @@ import android.widget.TextView;
 
 import com.goodl.aes.FooTools;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText editContent;
     private Button jiami;
     private Button jiemi;
     private EditText resultText;
+    private TextView changeFinalText;
+    private FooTools fooTools;
+    private Field finalTextField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fooTools = new FooTools();
+        try {
+            finalTextField = FooTools.class.getDeclaredField("finalText");
+            finalTextField.setAccessible(true);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(finalTextField, finalTextField.getModifiers() & ~Modifier.FINAL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         editContent = findViewById(R.id.editContent);
         jiami = findViewById(R.id.jiami);
         jiemi = findViewById(R.id.jiemi);
         resultText = findViewById(R.id.resultText);
+        changeFinalText = findViewById(R.id.changeFinalText);
         jiami.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String content = editContent.getText().toString();
                 String jiamiText = FooTools.method01(content);
                 resultText.setText(jiamiText);
+                setFinalTextFieldValue(jiamiText);
+                updateChangeFinalTextView();
             }
         });
         jiemi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String content = editContent.getText().toString();
-                resultText.setText(FooTools.method02(content));
+                String jiemiText = FooTools.method02(content);
+                resultText.setText(jiemiText);
+                setFinalTextFieldValue(jiemiText);
+                updateChangeFinalTextView();
             }
         });
+
+    }
+
+
+    /**
+     * 测试反射是否能设置final字段的值
+     * @param text
+     */
+    private void setFinalTextFieldValue(String text) {
+        try {
+            finalTextField.set(fooTools, text);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试反射是否能设置final字段的值
+     */
+    private void updateChangeFinalTextView() {
+        try {
+            String text = (String) finalTextField.get(fooTools);
+            changeFinalText.setText(text);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
